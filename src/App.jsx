8e289253bb0bbfde300baad7940ee4b4bb9c88b1
gabelapham <gabelapham@gmail.com>
2024-02-14 12:06:17 -0500

    more stuff

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,34 @@ import Background from './pages/Background-Folder/Background.jsx'
 import Spells from './pages/Spells-Folder/Spells.jsx'
 
 function App() {
+  const [statsVis, setStats] = useState(1);
+  const [backgroundVis, setBackground] = useState(0);
+  const [spellsVis, setSpells] = useState(0);
+
   return (
     <>
-    <div class="Heading">
-      <button>Stats</button>
-      <button>Background</button>
-      <button>Spells</button>
+    <div class="heading">
+      <h1>DnDBetter V2</h1>
+      <button onClick={() => 
+        {setStats(1);
+        setBackground(0);
+        setSpells(0)}}>Stats</button>
+      <button onClick={() => 
+        {setStats(0);
+        setBackground(1);
+        setSpells(0)}}>Background</button>
+      <button onClick={() => 
+        {setStats(0);
+        setBackground(0);
+        setSpells(1)}}>Spells</button>
     </div>
-    <div>
+    <div style={{ display: statsVis ? "block" : "none"}}>
       <Stats />
+    </div>
+    <div style={{ display: backgroundVis ? "block" : "none"}}>
       <Background />
+    </div>
+    <div style={{ display: spellsVis ? "block" : "none"}}>
       <Spells />
     </div>
     </>
--- src/pages/Stats-Folder/Stats.jsx
+++ src/pages/Stats-Folder/Stats.jsx
@@ -1,9 +1,21 @@
 import './Stats.css'
+import { useEffect, useState } from 'react'
+import users from './../../info/players.json'
+
+const endpoint = users
 
 function Stats() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+      const data = fetch(endpoint).then(res => res.json())
+      setUsers(data)
+  }, [])
+
     return (
       <>
         <h1>Stats Moment</h1>
+        <p>test{users[0]}</p>
       </>
     )
   }
