f72897ce6db310c356f74017e112416ddadbdf97
gabelapham <gabelapham@gmail.com>
2024-02-19 10:38:05 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+// Page Import
 import Stats from './pages/Stats-Folder/Stats.jsx'
 import Background from './pages/Background-Folder/Background.jsx'
 import Spells from './pages/Spells-Folder/Spells.jsx'
+
+// Picture Import
+import download_image from './assets/download_image.png'
 
 function App() {
   const [statsVis, setStats] = useState(1);
@@ -16,19 +21,23 @@ function App() {
     <div id='root'>
     <div id='heading'>
       <h1 id='title'>DnDBetter v2</h1>
-      <button id='tab_button' onClick={() => 
+      <button id='tab_button' title='Stats' onClick={() => 
         {setStats(1);
         setBackground(0);
         setSpells(0)}}>Stats</button>
-      <button id='tab_button' onClick={() => 
+      <button id='tab_button' title='Background' onClick={() => 
         {setStats(0);
         setBackground(1);
         setSpells(0)}}>Background</button>
-      <button id='tab_button' onClick={() => 
+      <button id='tab_button' title='Spells' onClick={() => 
         {setStats(0);
         setBackground(0);
         setSpells(1)}}>Spells</button>
     </div>
+    {/* On click, this should save the character sheet to a pdf for the user to download. */}
+    <button id='save_button' title='Save'><img id='save_image'src={download_image}/></button>
+
+    {/* Pages displayed when the individual button is clicked. */}
     <div style={{ display: statsVis ? "block" : "none"}}>
       <Stats />
     </div>
--- src/pages/Stats-Folder/Stats.jsx
+++ src/pages/Stats-Folder/Stats.jsx
@@ -5,9 +5,21 @@ import stats_image from './../../assets/stats_image.png'
 
 function Stats() {
 
+  const [characterName, setCharacterName] = useState("Enter name here...")
+
     return (
       <>
         <img src={stats_image} id='background_image'/>
+        <p id='char_name' contentEditable='false' onClick={() => {
+          var name = prompt("Insert your character's name.")
+          if (name.length() == 0) {
+            setCharacterName("Click to enter name...")
+          } else {
+            setCharacterName(name)
+          }
+        }}>{characterName}</p>
+        <button id='char_button' onClick={() => {
+        }}>Button.</button>
       </>
     )
   }
